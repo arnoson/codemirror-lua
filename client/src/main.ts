30bdeed8fed85068e98f355b823abba882736628
arnoson <hallo@arno-schlipf.info>
2022-03-10 19:50:15 +0100

    Modify codemirror language server plugin

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,12 +2,13 @@ import { basicSetup, EditorState, EditorView } from '@codemirror/basic-setup'
 import { lua } from '@codemirror/legacy-modes/mode/lua'
 import { StreamLanguage } from '@codemirror/stream-parser'
 import './style.css'
-import { languageServer } from 'codemirror-languageserver'
+// @ts-ignore
+import { languageServer } from './codemirror-languageserver'
 
 const fileName = 'test.lua'
 const luaLanguageServer = languageServer({
   serverUri: `ws://localhost:8080`,
-  rootUri: 'file:///',
+  // rootUri: 'file:///', (overwritten in `codemirror-languageserver.js, line 118)
   documentUri: `file:///${fileName}`,
   languageId: 'lua',
 })
